5249be16267184d17f97b309644b92ba054cca25
anurag-x-singh <105324949+anurag-x-singh@users.noreply.github.com>
2022-12-07 20:18:15 +0530

    Update and rename usp_insert_person.sql to usp_insert_test_person.sql

---
+++ Github-Actions/Business Rules/usp_insert_test_person.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE test.insert_persons 
+AS 
+BEGIN 
+
+SET NOCOUNT ON 
+
+TRUNCATE TABLE test.test_persons
+INSERT INTO test.test_persons VALUES (1, 'yellow', 'Apache', '202 N ave', 'New jersey');
+INSERT INTO test.test_persons VALUES (2, 'blue', 'Black', '202 E ave', 'New jersey');
+INSERT INTO test.test_persons VALUES (3, 'green', 'Gomea', '202 W ave', 'New jersey');
+INSERT INTO test.test_persons VALUES (4, 'table', 'RTR', '202 S ave', 'New jersey');
+
+END 
+
