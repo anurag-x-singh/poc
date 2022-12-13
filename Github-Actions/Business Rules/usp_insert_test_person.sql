1b0719c6cd5921e1fe646c26d11f45d857bbe854
anurag-x-singh <105324949+anurag-x-singh@users.noreply.github.com>
2022-12-14 01:11:53 +0530

    Update usp_insert_test_person.sql

--- Github-Actions/Business Rules/usp_insert_test_person.sql
+++ Github-Actions/Business Rules/usp_insert_test_person.sql
@@ -1,14 +1,4 @@
-CREATE PROCEDURE test.zzzzzzzzz_insert_persons 
-AS 
-BEGIN 
+CREATE VIEW test.yyyyy_persons_test AS
+SELECT top(100) * from test.f_plan
 
-SET NOCOUNT ON 
-
-TRUNCATE TABLE test.test_persons
-INSERT INTO test.test_persons VALUES (1, 'yellow', 'Apache', '202 N ave', 'New jersey');
-INSERT INTO test.test_persons VALUES (2, 'blue', 'Black', '202 E ave', 'New jersey');
-INSERT INTO test.test_persons VALUES (3, 'green', 'Gomea', '202 W ave', 'New jersey');
-INSERT INTO test.test_persons VALUES (4, 'table', 'RTR', '202 S ave', 'New jersey');
-
-END 
 
